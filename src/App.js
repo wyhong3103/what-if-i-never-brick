e399f5753a1b0a0aa91d136b0dd500c2d11921cf
wyhong3103 <wyhong3103@gmail.com>
2023-06-15 12:30:33 +0800

    Add dummy data & rating component

--- src/App.js
+++ src/App.js
@@ -1,2 +1,47 @@
+import './styles/App.css';
+import { useEffect, useState } from "react";
+import { RatingGraph } from "./components/RatingGraph";
+
 export const App = () => {
+    const [loading, setLoaing] = useState(false);
+    const values = [
+        {
+            x: new Date("2020-01-01"),
+            y: 300
+        },
+        {
+            x: new Date("2020-01-02"),
+            y: 1002
+        },
+        {
+            x: new Date("2020-01-03"),
+            y: 1505
+        },
+        {
+            x: new Date("2020-01-11"),
+            y: 1904
+        },
+        {
+            x: new Date("2025-02-11"),
+            y: 1904
+        },
+        {
+            x: new Date("2026-02-11"),
+            y: 3005
+        },
+    ];
+
+    return(
+        <>
+            <div class="result-container">
+                {
+                    loading === false ?
+                    <RatingGraph values={values}/>
+                    :
+                    //probably react spinner
+                    null
+                }
+            </div>
+        </>
+    ) 
 }
