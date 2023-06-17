477887d5b9f682bec8b4f9619ac76e9d565cbd08
wyhong3103 <wyhong3103@gmail.com>
2023-06-17 19:22:46 +0800

    Add api handler

---
+++ src/util/apiHandler.js
@@ -0,0 +1,9 @@
+export const apiHandler = (() => {
+    const API_BASE_URL = `https://codeforces.com/api/`;
+
+    const getContestData = async (contestID) => {
+        const res = await fetch(API_BASE_URL+`contest.ratingChanges?contestId=${contestID}`);
+        const ret = await res.json();
+        return ret.result;
+    }
+})()
