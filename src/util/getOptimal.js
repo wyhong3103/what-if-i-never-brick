62bb0bacd0e684acee2f5f0420f740794939b432
wyhong3103 <wyhong3103@gmail.com>
2023-06-18 19:52:07 +0800

    Add get optimal set of contests

---
+++ src/util/getOptimal.js
@@ -0,0 +1,30 @@
+import { apiHandler } from "./apiHandler";
+import { ratingCalculator } from "./ratingCalculator";
+
+export const getOptimal = async (handle) => {
+    let currentRating = 0;
+    // [id, name, date]
+    const contestList = await apiHandler.getContestList(handle);
+    const res = [];
+
+    for(let i = 0; i < contestList.length; i++){
+        const contestData = await apiHandler.getContestData(contestList[i][0]);
+        if (!i){
+            for(const i of contestData){
+                if (i.handle === handle){
+                    currentRating = Math.max(0, i.newRating);
+                }
+            }
+            if (currentRating > 0) res.push([currentRating, contestList[i][1], (new Date(0)).setUTCSeconds(contestList[i][2])]);
+        }
+        else{
+            const delta = ratingCalculator(handle, currentRating, contestData);
+            if (delta > 0){
+                currentRating += delta;
+                res.push([currentRating, contestList[i][1], (new Date(0)).setUTCSeconds(contestList[i][2])]);
+            }
+        }
+    }
+
+    return res;
+};
