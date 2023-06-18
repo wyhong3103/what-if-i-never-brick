0921fd9a41ce2f091095583bdf145110f028fa5e
wyhong3103 <wyhong3103@gmail.com>
2023-06-18 13:31:05 +0800

    Update rating calculator

--- src/util/ratingCalculator.js
+++ src/util/ratingCalculator.js
@@ -1,36 +1,27 @@
 export const ratingCalculator = (targetHandle, rating, contestants) => {
-    const MAX = 5000;
-    const seed = new Array(MAX * 2);
-    const delta = new Array(contestants.length);
-    const perf = new Array(contestants.length);
+    const RATING_MIN = -500;
+    const RATING_MAX = 5000;
+    const seed = new Array(RATING_MAX-RATING_MIN);
 
     const setRatingOfTarget = () => {
         for(const i of contestants){
             if (i.handle === targetHandle){
                 i.oldRating = rating;
             }
+            i.delta = 0;
         }
     }
 
     const precalcSeed = () => {
-        const winProb = new Array(MAX * 4);
-        for(let i = 0; i < MAX * 4; i++){
-            winProb[i] = 1 / (1 + Math.pow(10, (i-MAX*2) / 400));
+        const count = new Array(RATING_MAX-RATING_MIN).fill(0);
+        for(let i = 0; i < contestants.length; i++){
+            count[contestants[i].oldRating+Math.abs(RATING_MIN)]++;
         }
 
-        const count = [];
-        for(let i = 0; i < MAX*2; i++){
-            count.push(0);
-        }
-        for(const i of contestants) {
-            count[i.oldRating + MAX]++;
-        }
-        
-        for(let i = 0; i < 2 * MAX; i++){
-            seed[i] = 1;
-            for(let j = 0; j < 2 * MAX; j++) {
-                if (count[j] === 0) continue;
-                seed[i] += (count[j] - (j === i ? 1 : 0)) * winProb[i-j+MAX*2];
+        for(let i = RATING_MIN; i < RATING_MAX; i++){
+            seed[i+Math.abs(RATING_MIN)] = 1;
+            for(let j = RATING_MIN; j < RATING_MAX; j++){
+                seed[i+Math.abs(RATING_MIN)] += (count[j+Math.abs(RATING_MIN)] - (i === j ? 1 : 0)) * (1.0 / (1 + Math.pow(10, (i-j)/400.0)));
             }
         }
     };
@@ -39,47 +30,52 @@ export const ratingCalculator = (targetHandle, rating, contestants) => {
         for(let i = 0; i < contestants.length; i++){
             const actualRating = contestants[i].oldRating;
             const actualRank = contestants[i].rank;
-            const playerSeed = seed[actualRating+MAX];
-            const geometricMean = Math.pow(actualRank * playerSeed, 0.5);
+            const playerSeed = seed[actualRating+Math.abs(RATING_MIN)];
+            const geometricMean = Math.sqrt(actualRank * playerSeed);
 
-            let lo = 0, hi = MAX * 2 - 1;
+            let lo = RATING_MIN, hi = RATING_MAX-1;
             while (hi > lo){
                 const mid = lo + Math.floor((hi-lo+1)/2);
-                if (seed[mid] >= geometricMean) lo = mid;
+                if (seed[mid+Math.abs(RATING_MIN)] >= geometricMean) lo = mid;
                 else hi = mid-1;
             }
             
-            const expectedRating = lo - MAX; 
-            delta[i] = Math.floor((expectedRating - actualRating) / 2);
-            perf[i] = expectedRating;
+            const expectedRating = lo; 
+            contestants[i].delta = Math.floor((expectedRating - actualRating) / 2);
         }
     }
 
     const correctDelta = () => {
+        contestants.sort(
+            (a, b) => {
+                return b.oldRating - a.oldRating;
+            }
+        )
         let deltaSum = 0;
-        for(const i of delta){
-            deltaSum += i;
+        for(const i of contestants){
+            deltaSum += i.delta;
         }
 
         for(let i = 0; i < contestants.length; i++){
-            delta[i] += Math.floor( - deltaSum / (contestants.length-1));
+            contestants[i].delta += Math.floor( - deltaSum / (contestants.length)) - 1;
         }
 
         let s = Math.min(contestants.length, Math.floor(4 * Math.pow(contestants.length, 0.5)));
+
         let sumS = 0;
         for(let i = 0; i < s; i++){
-            sumS += delta[i];
+            sumS += contestants[i].delta;
         }
 
         for(let i = 0; i < contestants.length; i++){
-            delta[i] += Math.min(0, Math.max(Math.floor(-sumS / s), -10));
+            contestants[i].delta += Math.min(0, Math.max(Math.floor(-sumS / s), -10));
         }
     }
 
     const getTargetDelta = () => {
         for(let i = 0; i < contestants.length; i++){
             if (contestants[i].handle === targetHandle){
-                return delta[i];
+                return contestants[i].delta;
             }
         }
     }
