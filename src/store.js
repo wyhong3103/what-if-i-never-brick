9a7ced39d1217d4baa8f7ae75c1b0d894cfa2237
wyhong3103 <wyhong3103@gmail.com>
2023-06-19 18:18:29 +0800

    Add reducer to store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
+import appStateReducer from './reducers/appStateSlice';
 
 export default configureStore({
-  reducer: {}
+  reducer: {appState : appStateReducer}
 });
